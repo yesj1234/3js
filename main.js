146162c9c7f0f982c0d266d3badb5caa28548554
yesj1234 <86505279+yesj1234@users.noreply.github.com>
2024-02-20 13:55:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+// Constants
+const WHITE = new THREE.Color(0xffffff);
+const BLACK = new THREE.Color(0x000000);
+
 const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
+scene.background = WHITE;
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const gridHelper = new THREE.GridHelper(50, 50);
+scene.add(gridHelper);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -13,17 +24,23 @@ const camera = new THREE.PerspectiveCamera(
   0.01,
   1000
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 20, 50);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
+
+const material = new THREE.MeshPhysicalMaterial({});
+
 const loader = new GLTFLoader();
 loader.load(
-  "/sorceress.glb",
+  "/dynasty_clothe.glb",
   (gltf) => {
     console.log(gltf);
-    const model = gltf.scene;
+    const mesh = gltf.scene;
+    // model.castShadow = true;
     // model.scale.set(0.1, 0.1, 0.1);
-    scene.add(model);
+    scene.add(mesh);
+    renderer.render(scene, camera);
   },
   undefined,
   (error) => {
